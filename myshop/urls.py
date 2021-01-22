9a33d8abe0d437a870f35ebb171713c40d729a2b
Abhishek Yadav <abhisheky414@gmail.com>
2021-01-22 21:21:44 +0530

    Image problem

--- myshop/urls.py
+++ myshop/urls.py
@@ -26,5 +26,11 @@ urlpatterns = [
     path('', include('shop.urls', namespace='shop')),
 ]
 if settings.DEBUG:
-    url(r'^media/(?P<path>.*)$', serve,{'document_root':settings.MEDIA_ROOT}), 
-    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
+    from django.conf.urls.static import static
+    from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
+    urlpatterns +=staticfiles_urlpatterns()
+    urlpatterns +=static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
+    urlpatterns +=static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+   
+    
